8181be15008210080c291c571e4ddc02b2b20b58
ADITYA <107695376+nettech12@users.noreply.github.com>
2023-01-02 18:05:04 +0530

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -1,6 +1,8 @@
 apt-get update -y
 sudo apt install openssh-server -y
 apt-get install unzip wget -y 
+
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
 unzip ngrok-stable-linux-amd64.zip
+./ngrok authtoken 2BjZsYSiFLDHfSsvlLOPJUfmeLK_2ZvaLgWN1BbxottuPZV8E
 ./ngrok tcp 22
